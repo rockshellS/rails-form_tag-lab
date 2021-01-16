c3638bd76b6b149b6ad5c7751beb3ae221d19a89
rockshellS <spicer.rachelle@gmail.com>
2021-01-16 15:25:00 -0600

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+studnet1 = Student.create(first_name: "Evans", last_name: "Spicer")
+studnet2 = Student.create(first_name: "Remi", last_name: "Spicer")
+studnet3 = Student.create(first_name: "Jaden", last_name: "DeHate")
+studnet4 = Student.create(first_name: "Cooper", last_name: "Robinson")
